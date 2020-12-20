aa16437586b9c5c5719454b3033544f838cae336
Alla Karhopolova <alalasimys@gmail.com>
2020-12-21 00:34:09 +0200

    Добавляет скрипт для анимации при скролле

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,5 +1,32 @@
-const elements = document.querySelectorAll('.anim-show');
+(() => {
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
 
-elements.forEach(item => {
-  item.classList.add('active');
-});
+  const elements = document.querySelectorAll('.anim-show');
+  const elem = document.querySelectorAll('.animate__animated');
+
+  window.onscroll = () => {
+    elements.forEach(item => {
+      if (isElementInViewport(item)) {
+        item.classList.add('active');
+      }
+    });
+
+    elem.forEach(item => {
+      if (isElementInViewport(item)) {
+        item.classList.add('animate__heartBeat');
+      }
+    });
+  }
+})();
